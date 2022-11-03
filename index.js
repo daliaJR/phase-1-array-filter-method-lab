0f4aa552764f22e34b8a58de04859a757485a4cc
Dalia Jaber <daliajaber96@gmail.com>
2022-11-03 14:41:26 +0200

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Code your solution here
+// Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+function findMatching(arr,stringName){
+    let newArr = arr.filter((e)=>{       
+        if(e.toLowerCase() === stringName.toLowerCase())
+        return e;
+        
+    })
+    return newArr;
+}
+// console.log(findMatching(drivers,"Bobby"));
+
+function fuzzyMatch(arr,stringName){ //(drivers. 'Sa)
+    let newArr = arr.filter((e)=>{
+        let len = stringName.length;
+        
+        for(let i = 0;i<len;i++){
+            if(e.charAt(i).toLowerCase() ===  stringName.charAt(i).toLowerCase()){
+                return e;
+            }
+        }
+       
+    })
+    return newArr;
+}
+
+// console.log(fuzzyMatch(drivers,"Sa"));
+
+function matchName(arr,stringName){
+
+    let newArr = arr.filter((e)=>{
+        if(e.name === stringName){
+            return e;
+        }
+        
+    });
+    return newArr;
+}
+
